88e1897c0d877caa0768361f3735dce892d348ba
Jake Berg <jacobvictorberg@gmail.com>
2018-06-26 12:57:45 -0400

    trying again

--- test.js
+++ test.js
@@ -16,18 +16,19 @@ app.get('/users', (req, res) => {
     });
 });
 
-app.get('/users/:id', (req, res) => {
-   res.send(req.params)
-});
-
-app.post('/users', (req,res) => {
+app.post('/users', (req, res) => {
     const text = 'INSERT INTO users (username, bio) VALUES ($1, $2) RETURNING *';
-    const values = ['kenzie', 'Kenzie Academy is a user experience design and coding school in Indianapolis, Indiana. Our 6-month to 2-year program with 1-year paid apprenticeship is a new alternative to traditional colleges and short-term coding bootcamps.'];
+    const values = [req.body.username, req.body.bio];
     client.query(text, values, (err, result) => {
-        res.send(result.rows)
+        res.send(result.rows[0]);
     });
 });
 
+app.get('/users/:id', (req, res) => {
+    client.query(`SELECT * FROM users WHERE id = ${req.params.id}`, (err, result) => {
+        res.send(result.rows);
+    });
+});
 // start a server that listens on port 3000 and connects the sql client on success
 app.listen(3000, () => {
     client.connect();
